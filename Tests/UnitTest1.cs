fbb1db6a06132bf72e4e7963859aa726b3db8d08
yuriusV <yuravashchenko15@gmail.com>
2019-07-17 03:30:41 +0300

    Added some tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,18 +1,56 @@
 using NUnit.Framework;
+using SaleTerminal;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.Linq;
 
 namespace Tests
 {
-    public class Tests
+    public class PointOfSaleTerminalTest
     {
+		private PointOfSaleTerminal _terminal;
+
         [SetUp]
         public void Setup()
         {
+			_terminal = new PointOfSaleTerminal();
         }
 
         [Test]
-        public void Test1()
+        public void EmptyIsCalculatedToZero()
         {
-            Assert.Pass();
+			_terminal.SetPricing(Enumerable.Empty<Price>());
+            Assert.That(_terminal.CalculateTotal() == 0);
+        }
+
+		[Test]
+        public void OneProductIsCalculatedCorrect()
+        {
+			string productName = "A";
+			decimal productPrice = 1.2m;
+
+			_terminal.SetPricing(new List<Price> {
+				new SimplePrice(productName, productPrice)
+			});
+			_terminal.Scan(productName);
+
+            Assert.That(_terminal.CalculateTotal() == productPrice);
+        }
+
+		[Test]
+        public void ProductPackIsCalculatedCorrect()
+        {
+			string productName = "A";
+			int countProducts = 3;
+			decimal productPrice = 1.2m;
+
+			_terminal.SetPricing(new List<Price> {
+				new ProductPackPrice(productName, countProducts, productPrice)
+			});
+			_terminal.Scan(productName);
+
+            Assert.That(_terminal.CalculateTotal() == productPrice);
         }
     }
 }
